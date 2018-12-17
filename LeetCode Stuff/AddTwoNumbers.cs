421b7e52800aa899ef616602dca4c974c3a36496
James Platt <James.Platt@Neptune.com>
2018-12-17 18:37:57 +0000

    Added "Add Two Numbers" Tests

---
+++ LeetCode Stuff/AddTwoNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Stuff
+{
+    public class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+
+            public static ListNode GetList(int[] values)
+            {
+                var output = new AddTwoNumbersSolution.ListNode(values[0]);
+                var curNode = output;
+                for (int i = 1; i < values.Count(); i++)
+                {
+                    curNode.next = new AddTwoNumbersSolution.ListNode(values[i]);
+                    curNode = curNode.next;
+                }
+                return output;
+            }
+
+            public override string ToString()
+            {
+                ListNode list = this;
+                var sb = new StringBuilder();
+                while (list != null)
+                {
+                    sb.Append(list.val);
+                    list = list.next;
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ Tests/AddTwoNumbersTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_Stuff;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class AddTwoNumbersTests
+    {
+        private AddTwoNumbersSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new AddTwoNumbersSolution();
+        }
+
+        [TestMethod]
+        public void TestGetList()
+        {
+            var list = AddTwoNumbersSolution.ListNode.GetList(new int[] { 0, 1, 2, 3 });
+            Assert.AreEqual(list.val, 0);
+            Assert.AreEqual(list.next.val, 1);
+        }
+
+        [TestMethod]
+        public void TestListToString()
+        {
+            var list = AddTwoNumbersSolution.ListNode.GetList(new int[] { 0, 1, 2, 3 });
+            Assert.AreEqual(list.ToString(), "0123");
+        }
+
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            var listA = AddTwoNumbersSolution.ListNode.GetList(new int[] { 1, 0, 0 });
+            var listB = AddTwoNumbersSolution.ListNode.GetList(new int[] { 1, 0, 0 });
+
+            var result = _solution.AddTwoNumbers(listA, listB);
+            Assert.AreEqual(result.ToString(), "200");
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersOverflow()
+        {
+
+            var listA = AddTwoNumbersSolution.ListNode.GetList(new int[] { 9, 0, 0 });
+            var listB = AddTwoNumbersSolution.ListNode.GetList(new int[] { 5, 0, 0 });
+
+            var result = _solution.AddTwoNumbers(listA, listB);
+            Assert.AreEqual(result.ToString(), "1400");
+        }
+    }
+}
